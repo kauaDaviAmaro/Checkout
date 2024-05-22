3aa8ac12343895ad381fe99ca4f230a7e8959f7e
Kauã Davi <kauadaviamaro@gmail.com>
2024-05-22 17:31:39 -0300

    Update files
    - back/obj/Debug/net8.0/Checkout.AssemblyInfo.cs
    - back/obj/Debug/net8.0/Checkout.AssemblyInfoInputs.cache
    - back/obj/Debug/net8.0/Checkout.GeneratedMSBuildEditorConfig.editorconfig
    - back/obj/Debug/net8.0/Checkout.assets.cache

--- back/obj/Debug/net8.0/Checkout.AssemblyInfo.cs
+++ back/obj/Debug/net8.0/Checkout.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cb043b519b4df5f53d2fb1ea9c60c9a463253ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Checkout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
